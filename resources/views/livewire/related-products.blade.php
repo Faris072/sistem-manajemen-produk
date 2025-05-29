95e769157d08ec2168e9c850f0e1719b134acd3c
faris072 <farisbos.mfs@gmail.com>
2025-05-29 15:00:29 +0700

    chore: show view detail and repair style

--- resources/views/livewire/related-products.blade.php
+++ resources/views/livewire/related-products.blade.php
@@ -12,9 +12,9 @@
                 @endif
                 <div class="p-3 flex-grow">
                     <h4 class="text-lg font-medium">{{ $productItem->name }}</h4> <p class="text-gray-700">Rp {{ number_format($productItem->price, 0, ',', '.') }}</p> </div>
-                {{-- <div class="p-3 border-t">
-                    <a href="{{ route('filament.admin.resources.products.view', $productItem->id) }}" class="text-blue-600 hover:underline">View Details</a>
-                </div> --}}
+                <a href="{{ route('filament.admin.resources.products.view', $productItem->id) }}" class="cursor-pointer p-3 border-t flex items-center justify-center">
+                    <span class="text-blue-600 hover:underline">View Details</span>
+                </a>
             </div>
         @empty
             <p>No other products found in this category.</p>
